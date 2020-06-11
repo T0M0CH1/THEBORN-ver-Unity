eb0f6b131bf61d73277a224bfd962022b3101745
HarutoNagashima <19d3170013nh@edu.tech.ac.jp>
2020-06-11 20:12:15 +0900

    タイトル・ムービー・ゲーム画面への遷移・オプション画面作成

---
+++ Assets/ProgramerFile/Nagashima/Scripts/MovieSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MovieSceneController : MonoBehaviour
+{
+    private float step_time;  // 経過時間カウント
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        step_time = 0.0f; // 経過時間初期化
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+
+        // 5秒後に画面遷移
+        if(step_time>=5.0f)
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
