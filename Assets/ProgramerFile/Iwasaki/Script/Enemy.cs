61c619b1cee093b15b3306aa3d79eae3192751b3
岩崎友哉 <tomoti2525@yahoo.co.jp>
2020-06-12 19:39:49 +0900

    虫の処理を少し進めた。

---
+++ Assets/ProgramerFile/Iwasaki/Script/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] enemyImage;
+    private Rigidbody2D rb2D;
+    void Start()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = enemyImage[0];
+        rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //オブジェクトから下側にRayを伸ばす
+        Ray2D ray = new Ray2D(transform.position, Vector2.down);
+
+        int layerMask = LayerMask.GetMask(new string[] { "Player" });
+        int raydistance = 10;
+
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 10f, layerMask);
+        Debug.DrawRay(ray.origin, ray.direction * raydistance, Color.red);
+        if (hit.collider)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = enemyImage[1];
+            rb2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }                
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            this.gameObject.transform.localScale = new Vector3(0.35f, 0.15f, 0);
+            StartCoroutine(waitTime(0.5f));
+        }
+    }
+
+    IEnumerator waitTime(float time)
+    {
+        new WaitForSeconds(time);
+        yield break;
+    }
+}
---
+++ Assets/ProgramerFile/Iwasaki/Script/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    //プライベート変数
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+
+    void Start()
+    {
+        //コンポーネントのインスタンスを捕まえる
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //キー入力されたら行動する
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+        if (horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(-0.12f, 0.12f, 0.12f);
+            //anim.SetBool("walk", true);
+            xSpeed = speed;
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(0.12f, 0.12f, 0.12f);
+            //anim.SetBool("walk", true);
+            xSpeed = -speed;
+        }
+        else
+        {
+            //anim.SetBool("walk", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+    }
+}
