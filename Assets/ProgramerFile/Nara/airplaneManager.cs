dd5d2b035ec40f9c1168e8ba336f393534b2c250
harutonara <haruto.nara.2000@gmail.com>
2020-07-17 14:15:19 +0900

    背景のループ仕様の作成(終了)

---
+++ Assets/ProgramerFile/Nara/airplaneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airplaneManager : MonoBehaviour
+{
+
+    int positionX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        positionX++;
+
+        //プレイヤーを移動
+        transform.position = new Vector3(positionX, -4, -1);
+
+
+
+    }
+}
